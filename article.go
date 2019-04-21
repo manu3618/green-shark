4760eaa4ff5d79fe8add71459e38004dba8d6eda
manu3618 <manu+github@hbrt.eu>
2019-04-21 17:57:10 +0200

    [TESTS] Add bag of word tests

--- article.go
+++ article.go
@@ -16,11 +16,16 @@ type Article struct {
 	title   string
 }
 
-func (art Article) toWordVector() map[string]int {
+
+func (art Article) ToWordVector() map[string]int {
 	res := make(map[string]int)
 	var ok bool
 	var count int
+    var ponct = []string{".", ";", ","}
 	text := strings.ToLower(art.content)
+    for _, char := range ponct {
+        text = strings.ReplaceAll(text, char, " ")
+    }
 	for _, w := range strings.Fields(text) {
 		count, ok = res[w]
 		if ok {
@@ -31,6 +36,7 @@ func (art Article) toWordVector() map[string]int {
 	}
 	return res
 }
+
 
 func (art Article) String() string {
 	return fmt.Sprintf(
---
+++ article_test.go
@@ -0,0 +1,63 @@
+package article
+
+import (
+	"testing"
+	"time"
+)
+
+type ArticleTest struct {
+	article Article
+	bow     map[string]int //Expected result of Bag-of-word representation
+}
+
+var base_time = time.Date(2019, time.January, 10, 23, 0, 0, 0, time.UTC)
+var samples = []ArticleTest{
+	{
+		article: Article{
+			date:    base_time,
+			title:   "Title",
+			content: "This article is really interresting.",
+		},
+		bow: map[string]int{
+			"this":         1,
+			"article":      1,
+			"is":           1,
+			"really":       1,
+			"interresting": 1},
+	},
+	{
+		article: Article{
+			date:    base_time.Add(19 * time.Minute),
+			title:   "Youhou",
+			content: "This article is not really interresting.",
+		},
+		bow: map[string]int{
+			"this":         1,
+			"not":          1,
+			"article":      1,
+			"is":           1,
+			"really":       1,
+			"interresting": 1},
+	},
+}
+
+func TestBow(t *testing.T) {
+	for _, smpl := range samples {
+		got := smpl.article.ToWordVector()
+
+		for k, _ := range smpl.bow {
+			if _, ok := got[k]; !ok {
+				t.Errorf("word %q expected.", k)
+			}
+		}
+		for k, v := range got {
+			expected_value, ok := smpl.bow[k]
+			if !ok {
+				t.Errorf("word %q not in  expected words", k)
+			}
+			if expected_value != v {
+				t.Errorf("key %q: expected %d, got %d", k, expected_value, v)
+			}
+		}
+	}
+}
