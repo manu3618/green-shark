5de3c0571d3370b5007b20be08eb1c4f3c13683b
manu3618 <manu+github@hbrt.eu>
2019-05-05 16:17:51 +0200

    [TEST] Fix tests RSS-->Articles

--- article_test.go
+++ article_test.go
@@ -52,7 +52,7 @@ func TestBow(t *testing.T) {
 		for k, v := range got {
 			expected_value, ok := expected[k]
 			if !ok {
-				t.Errorf("word %q not in  expected words", k)
+				t.Errorf("word %q not in expected words", k)
 			}
 			if expected_value != v {
 				t.Errorf("key %q: expected %d, got %d", k, expected_value, v)
@@ -62,11 +62,32 @@ func TestBow(t *testing.T) {
 }
 
 func TestHtmlArticle(t *testing.T) {
+	// Transform HTML to articles
 	input, err := ioutil.ReadFile("testdata/manu.hbrt.eu.xml")
 	if err != nil {
 		log.Fatal(err)
 	}
+	actual_arts := HtmlToArticle(input)
 
-    //TODO compare HtmlToArticle(input) and test data
-	HtmlToArticle(input)
+	// Retrieve expectedarticles
+	input, err = ioutil.ReadFile("testdata/articles.manu.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	expected_articles := make(map[string]Article)
+	var read_articles []Article
+	err = json.Unmarshal(input, &read_articles)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, art := range read_articles {
+		expected_articles[art.Id] = art
+	}
+
+	// Compare both
+	for _, art := range actual_arts {
+		if art != expected_articles[art.Id] {
+			t.Errorf("Article %q not as expected", art.Id)
+		}
+	}
 }
